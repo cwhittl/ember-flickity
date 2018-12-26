1b326433dfd3791c9fe9328f2881af6bd685f956
Chris Whittle <cwhittl@gmail.com>
2018-12-26 08:08:54 -0600

    added loading style

--- addon/components/em-flickity.js
+++ addon/components/em-flickity.js
@@ -6,9 +6,12 @@ import layout from '../templates/components/em-flickity';
 export default Component.extend({
   layout,
   classNames: ["flickity-wrapper"],
+  classNameBindings: ['loaded:loaded:loading'],
   _widget: null,
   showSlides: false,
   events: null,
+
+  loaded: false,
 
   // some default flickity settings
   cellAlign: "center",
@@ -106,6 +109,9 @@ export default Component.extend({
       if (eventFunc) {
         eventHandlers[key] = (...args) => {
           run.later(() => {
+            if (key === 'ready') {
+              set(this, 'loaded', true);
+            }
             const $widget = get(this, "_widget").data("flickity");
             eventFunc(...args, $widget);
           },0);
